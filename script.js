6d3139b35f5e212b5e6083fb98edf98d7f79bc51
Gibson <Gibson570@hotmail.com>
2024-10-01 02:06:13 +0100

    Added the different pomodoro states

--- script.js
+++ script.js
@@ -5,11 +5,30 @@ let currentCycle = 0;
 let isActive = false;
 let currentTime = workLength * 60;
 
+const CYCLE_ORDER = [
+  ['work', workLength],
+  ['short break', shortBreakLength],
+  ['work', workLength],
+  ['short break', shortBreakLength],
+  ['work', workLength],
+  ['long break', longBreakLength]
+];
+
 const START_STOP_BUTTON = document.getElementById('StartStopButton');
 const SKIP_BUTTON = document.getElementById('SkipButton');
 const TIMER_TEXT = document.getElementById('TimerText');
 const POMODORO_CYCLE = document.getElementById('PomodoroCycle');
 const POMODORO_COUNT = document.getElementById('PomodoroCount');
+
+function IncrementCycleCount() {
+  currentCycle += 1;
+  if (currentCycle >= CYCLE_ORDER.length) {
+    currentCycle = 0;
+  }
+  POMODORO_CYCLE.innerText = CYCLE_ORDER[currentCycle][0];
+  POMODORO_COUNT.innerText = `${Math.floor(currentCycle / 3) + 1}`;
+  currentTime = CYCLE_ORDER[currentCycle][1] * 60;
+}
 
 // Timer code to run every second
 setInterval(() => {
@@ -21,6 +40,10 @@ setInterval(() => {
     }
     currentTime -= 1;
     TIMER_TEXT.innerText = `${minutesLeft}:${secondsLeft}`;
+    if (currentTime <= 0) {
+      isActive = false;
+      IncrementCycleCount();
+    }
   }
 }, 1000);
 
